0df92e5b1da6d0545a0023733b3e0f65028b848f
aryapatel69 <2002297@apps.nsd.org>
2025-05-20 16:31:37 +0000

    sigma

---
+++ js.js
@@ -0,0 +1,14 @@
+var LIST = [
+" frozen burritoes",
+" skbididi dop dop",
+" banana foster",
+" egg mcmuffin wtih egg"
+
+]
+
+
+
+for( let i=0; i < 3; i++){
+    alert(" frozen burritoes" + list[i]);
+
+}
